9e2c3825d76dbb04002e2d89bff522ae8c75c27f
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-08 20:06:50 -0500

    q4 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -133,15 +133,15 @@ let sumAndProduct =(a, b) => {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-console.log(sumAndProduct(3, 9));
+// console.log(sumAndProduct(3, 9));
 
 
-let message = function(name) {
+let message = (name) => {
   return `Hello, ${name}!`;
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(message('Allie'));
+console.log(message('Allie'));
 
 
 let Student = function(name, age, hometown) {
