db9e1c34f7bb89a5629fcf664f3df6835a78f7c7
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-08 20:20:09 -0500

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -188,17 +188,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// Object { name: "Joe", age: "Schmoe", hometown: 100 }
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// Window http://127.0.0.1:5500/starter-code/
 // 3. Explain why "this" is different when an arrow function is used.
-//
+//  because this only uses the value of their nearest scope (the closest function)
