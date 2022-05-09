d6cf926268587483d2c827d6a095d49fa42edf96
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-08 20:04:25 -0500

    1 2 3 done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,5 +1,4 @@
-'use strict';
-
+'use strict';;
 // STEP 1
 // This is a standard function expression. You may also be familiar with function declarations, which begin with the "function" keyword.
 const theOldWay = function(course) {
@@ -108,15 +107,14 @@ const newObject = array => ({
 
 
 
-let sum = function(a, b, c, d) {
-  return a + b + c + d;
-};
+let sum = (a, b, c, d) => a + b + c + d;
+
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // console.log(sum(1, 2, 3, 4));
 
 
-let objectLit = function() {
+const objectLit = () => {
   return {
     key1: 'value1',
     key2: 'value2',
@@ -128,14 +126,14 @@ let objectLit = function() {
 // console.log(objectLit());
 
 
-let sumAndProduct = function(a, b) {
+let sumAndProduct =(a, b) => {
   let sum = a + b;
   let product = a * b;
   return [sum, product];
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(sumAndProduct(3, 9));
+console.log(sumAndProduct(3, 9));
 
 
 let message = function(name) {
